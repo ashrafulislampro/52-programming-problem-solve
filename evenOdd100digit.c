5af59a95e0c41aec052090bfdd85e2c8063685a1
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-02-28 00:02:44 +0600

    even odd with 100 digit

--- evenOdd100digit.c
+++ evenOdd100digit.c
@@ -1,25 +1,53 @@
+// solution one
 #include <stdio.h>
 
 int main()
 {
     int T, i;
     char n[101];
-    printf("Enter a line number =");
     scanf("%d", &T);
 
     for (i = 1; i <= T; i++)
     {
-        printf("Enter a number =");
         scanf("%s", &n);
+        int size = strlen(n);
 
-        if (n % 2 == 0)
+        if (n[size - 1] % 2 == 0)
         {
-            printf("even = %d\n", n);
+            printf("even\n");
         }
         else
         {
-            printf("odd = %d\n", n);
+            printf("odd\n");
         }
     }
     return 0;
 }
+
+// solution 2
+// #include <stdio.h>
+
+// int main()
+// {
+//     int T, i, size;
+//     char n[101];
+//     printf("Enter a line number =");
+//     scanf("%d", &T);
+
+//     for (i = 1; i <= T; i++)
+//     {
+//         printf("Enter a number =");
+//         scanf("%s", &n);
+//         size = strlen(n);
+
+//         if (n[size - 1] % 2 == 0)
+//         {
+//             printf("even = %s\n", n);
+//         }
+//         else
+//         {
+//             printf("odd = %s\n", n);
+//         }
+//     }
+//     return 0;
+// }
