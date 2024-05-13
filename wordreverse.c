0cf11b669def02af6f9650752fc02d0c4fcd2d34
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-05-13 22:51:30 +0600

    word reverse problem solve

--- wordreverse.c
+++ wordreverse.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_reverse(char str[])
+{
+    int i;
+    for (i = strlen(str) - 1; i >= 0; i--)
+    {
+        printf("%c", str[i]);
+    }
+}
 int main()
 {
     int i, k, T, s_len;
@@ -11,7 +19,7 @@ int main()
     {
         scanf(" %[^\n]", str);
         s_len = strlen(str);
-        for (i = 0, k = 0; i > s_len; i++)
+        for (i = 0, k = 0; i < s_len; i++)
         {
             if (str[i] != ' ')
             {
@@ -21,14 +29,16 @@ int main()
             else if (k > 0)
             {
                 word[k] = '\0';
-                printf("%s\n", word);
+                print_reverse(word);
+                printf(" ");
                 k = 0;
             };
         }
         if (k > 0)
         {
             word[k] = '\0';
-            printf("%s\n", word);
+            print_reverse(word);
+            printf("\n");
         }
     }
     return 0;
