abc910c6419186bd0f212874ccab37efef758285
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-06-13 07:51:23 +0600

    prime number problem is done

--- prime.c
+++ prime.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <math.h>
+#define size 100001
+char arr[size];
+
+void seive()
+{
+    int i, root, j;
+    for (i = 2; i < size; i++)
+    {
+        arr[i] = 1;
+    }
+    root = sqrt(size);
+    for (i = 2; i <= root; i++)
+    {
+        if (arr[i] == 1)
+        {
+            for (j = 2; i * j < size; j++)
+            {
+                arr[i * j] = 0;
+            }
+        }
+    }
+}
 
 int main()
 {
     int a, b, T, count;
+    seive();
     scanf("%d", &T);
 
     while (T--)
@@ -11,12 +35,9 @@ int main()
         scanf("%d%d", &a, &b);
         for (int i = a; i <= b; i++)
         {
-            for (int j = 2; j * j <= b; j++)
+            if (arr[i] == 1)
             {
-                if (i % j == 0)
-                {
-                    break;
-                }
+                count++;
             }
         }
         printf("%d\n", count);
