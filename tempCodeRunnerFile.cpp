1b7b2ce024ee643f3ada6be2ed6b0def88881105
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-07-04 10:33:34 +0600

    addedd readme.md file

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+ // if (temp == sum)
+        // {
+        //     cout << temp << sum << " is an armstrong number" << endl;
+        // }
+        // else
+        // {
+        //     cout << temp << sum << " is not an armstrong number" << endl;
+        // }
