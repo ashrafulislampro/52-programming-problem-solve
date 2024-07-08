64c8eaa834ed903b21abf347e024dcb2149582b6
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-07-08 07:56:23 +0600

    run rate problem is solved

--- run_rate1_10.cpp
+++ run_rate1_10.cpp
@@ -5,18 +5,24 @@ using namespace std;
 int main()
 {
     int T, r1, r2, B;
-    double cur_run_rate, req_run_rate;
+    float cur_run_rate, req_run_rate;
 
     cin >> T;
     while (T--)
     {
-        cin >> r1 >> r2 >> B;        
+        cin >> r1 >> r2 >> B;
 
-        cur_run_rate = ((r2 * 1.0) / (300 - B)) * 6;
-        req_run_rate = (((r1 - r2 + 1) * 1.0) / B ) * 6;
-        cout << fixed << showpoint;
-        cout << setprecision(2);
-        cout << cur_run_rate << " " << req_run_rate << endl;
+        cur_run_rate = ((r2 * 1.0) / (300 - B)) * 6.0;
+        if (r1 < r2)
+        {
+            req_run_rate = 0.00;
+        }
+        else
+        {
+            req_run_rate = (((r1 - r2 + 1) * 1.0) / B) * 6.0;
+        }
+
+        cout << fixed << setprecision(2) << cur_run_rate << " " << fixed << setprecision(2) << req_run_rate << endl;
     }
 
     return 0;
