7bb1bec24470e38e9f4854dfda09345ababf3f7b
ashrafulislampro <ashrafulislam1659@gmail.com>
2024-07-11 00:09:01 +0600

    coalition of array problem is done

--- coalition_of_array_47.cpp
+++ coalition_of_array_47.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
-#include <cstdio>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main(){
-    int T, size, num;
-    cin>>T;
-    while(T--){
-        scanf("%d", &size);
-        
+int main()
+{
+    int T, size, num, len, i;
+    cin >> T;
+    while (T--)
+    {
+        vector<int> arr;
+        cin >> size;
+        for ( i = 0; i < size; i++)
+        {
+            cin >> num;
+            arr.push_back(num);
+        }
+        cin >> size;
+        for ( i = 0; i < size; i++)
+        {
+            cin >> num;
+            arr.push_back(num);
+        }
+        sort(arr.begin(), arr.end());
+        len = arr.end() - arr.begin();
+        for ( i = 0; i < len; i++)
+        {
+            cout << arr[i];
+            if (i < len - 1)
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
     }
     return 0;
 }
